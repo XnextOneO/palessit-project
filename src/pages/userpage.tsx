28e66d80c3bc92f680c125d957b2ab309733e1e3
XnextOneO <xnextone@gmail.com>
2023-07-13 11:08:03 +0300

    fix userpage

--- src/pages/userpage.tsx
+++ src/pages/userpage.tsx
@@ -1,8 +1,10 @@
-import {useEffect, useState} from 'react';
 import styled from "styled-components";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import {useQuery} from "react-query";
+import {AuthStorageService} from "../services/auth-storage.service.ts";
+import {ApiClient} from "../services/api-client.ts";
 
 
 const DeliverAddressWrapper = styled.div`
@@ -14,7 +16,8 @@ const DeliverAddressWrapper = styled.div`
 
 const UserDataWrapper = styled.div`
   display: flex;
-  gap: 12.688rem`
+  gap: 12.688rem;
+`
 
 const HouseApartmentFloorWrapper = styled.div`
   display: flex;
@@ -28,57 +31,62 @@ const ContactUserData = styled.div`
 
 
 function Userpage() {
-    const [name, setName] = useState(null);
-    const [email, setEmail] = useState(null);
 
-    useEffect(() => {
+    const query = useQuery(
+        'whoAmI',
+        async () => {
+            const token = AuthStorageService.getToken();
+            if (token) {
+                return await ApiClient.whoAmI(token);
+            } else {
+                return Promise.resolve(undefined);
+            }
+        });
 
-        fetch('https://63025c76c6dda4f287b8755e.mockapi.io/api/user/1')
-            .then(response => response.json())
-            .then(data => {
-                setName(data.name);
-                setEmail(data.email);
-            })
-            .catch(error => console.error(error));
-    }, []);
 
     return (
         <UserDataWrapper>
+            {
+                query.data ?
 
+                    <UserDataWrapper><DeliverAddressWrapper>
+                        <Typography variant="h4" color="#0F56B3">
+                            Данные доставки
+                        </Typography>
+                        <TextField id="outlined-basic" label="Населённый пункт" variant="outlined"
+                                   sx={{width: '12.9rem'}}/>
+                        <TextField id="outlined-basic" label="Улица" variant="outlined" sx={{width: '12.9rem'}}/>
+                        <HouseApartmentFloorWrapper>
+                            <TextField id="outlined-basic" label="Дом" variant="outlined" sx={{width: '4.1rem'}}/>
+                            <TextField id="outlined-basic" label="Квартира" variant="outlined" sx={{width: '6.5rem'}}/>
+                            <TextField id="outlined-basic" label="Этаж" variant="outlined" sx={{width: '5rem'}}/>
+                        </HouseApartmentFloorWrapper>
+                        <TextField
+                            id="outlined-basic"
+                            label="Комментарий к заказу"
+                            multiline
+                            rows={5}
+                            variant="outlined"
+                        />
 
-            <DeliverAddressWrapper>
-                <Typography variant="h4" color="#0F56B3">
-                    Данные доставки
-                </Typography>
-                <TextField id="outlined-basic" label="Населённый пункт" variant="outlined" sx={{width: '12.9rem'}}/>
-                <TextField id="outlined-basic" label="Улица" variant="outlined" sx={{width: '12.9rem'}}/>
-                <HouseApartmentFloorWrapper>
-                    <TextField id="outlined-basic" label="Дом" variant="outlined" sx={{width: '4.1rem'}}/>
-                    <TextField id="outlined-basic" label="Квартира" variant="outlined" sx={{width: '6.5rem'}}/>
-                    <TextField id="outlined-basic" label="Этаж" variant="outlined" sx={{width: '5rem'}}/>
-                </HouseApartmentFloorWrapper>
-                <TextField
-                    id="outlined-basic"
-                    label="Комментарий к заказу"
-                    multiline
-                    rows={5}
-                    variant="outlined"
-                />
+                    </DeliverAddressWrapper>
+                        <ContactUserData>
+                            <Typography variant="h4" color="#0F56B3">
+                                Контактные данные
+                            </Typography>
+                            <div>
+                                Name: {query.data.email}
+                                <br/>
+                                Email: {query.data.name}
+                            </div>
+                            <TextField id="outlined-basic" label="Телефон (+375...)" variant="outlined"
+                                       sx={{width: '15rem'}}/>
+                            <Button variant="contained">Сохранить</Button>
+                        </ContactUserData></UserDataWrapper>
 
-            </DeliverAddressWrapper>
-            <ContactUserData>
-                <Typography variant="h4" color="#0F56B3">
-                    Контактные данные
-                </Typography>
-                <div>
-                    Name: {name}
-                    <br/>
-                    Email: {email}
-                </div>
-                <TextField id="outlined-basic" label="Телефон (+375...)" variant="outlined" sx={{width: '15rem'}}/>
-                <Button variant="contained">Сохранить</Button>
-            </ContactUserData>
-
+                    :
+                    <div>Войдите в аккаунт</div>
+            }
         </UserDataWrapper>
     );
 }
