42f8409e7ca78f1eaf85a03c5d2d2a889e2f17d6
XnextOneO <xnextone@gmail.com>
2023-07-17 17:31:09 +0300

    refactor delete unused files

--- src/pages/catalog.tsx
+++
@@ -1,9 +0,0 @@
-import Typography from "@mui/material/Typography";
-
-export default function Catalog() {
-    return(
-        <Typography variant="h3" gutterBottom>
-            Каталог
-        </Typography>
-    )
-}
