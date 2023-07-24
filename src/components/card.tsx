74187ea7e9490aa53aa62c5eaa0801727ce0eb11
XnextOneO <xnextone@gmail.com>
2023-07-24 17:10:23 +0300

    fix product card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,6 +1,5 @@
-
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export interface Product {
     id: number;
@@ -9,10 +8,14 @@ export interface Product {
     image: string;
 }
 
-export default function ProductCardComponent({product}: { product: Product }): JSX.Element {
+export default function ProductCardComponent({
+                                                 product,
+                                             }: {
+    product: Product;
+}): JSX.Element {
     return (
         <ProductCard to={`/product/${product.id}`}>
-            <ProductImage src={product.image} alt={product.name}/>
+            <ProductImage src={product.image} alt={product.name} />
             <ProductInfo>
                 <ProductName>{product.name}</ProductName>
                 <ProductPrice>${product.price}</ProductPrice>
@@ -21,17 +24,16 @@ export default function ProductCardComponent({product}: { product: Product }): J
     );
 }
 
-
-
-
 const ProductCard = styled(Link)`
-  height: 25rem;
+  height: 22rem;
   margin-bottom: 2rem;
   box-shadow: 0 0 1rem rgba(0, 0, 0, 0.2);
   transition: all 0.3s ease-in-out;
   cursor: pointer;
   text-decoration: none;
   border-radius: 12px;
+  width: 16rem;
+  position: relative;
 
   &:hover {
     transform: scale(1.05);
@@ -41,12 +43,20 @@ const ProductCard = styled(Link)`
 
 const ProductImage = styled.img`
   width: 100%;
-  height: 15rem;
+  height: 100%;
   object-fit: cover;
+  border-radius: 12px;
 `;
 
 const ProductInfo = styled.div`
-  padding: 2rem;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 14rem;
+  padding: 1rem;
+  color: white;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 0 0 12px 12px;
 `;
 
 const ProductName = styled.h3`
@@ -56,5 +66,3 @@ const ProductName = styled.h3`
 const ProductPrice = styled.p`
   margin-bottom: 0;
 `;
-
-
