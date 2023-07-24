7086a4d20448e83e68320db1900aec87584d408f
XnextOneO <xnextone@gmail.com>
2023-07-24 13:00:46 +0300

    feat add sort to mainpage

--- src/pages/products-page.tsx
+++ src/pages/products-page.tsx
@@ -1,5 +1,5 @@
-import {useEffect, useRef, useState} from "react";
-import ProductCardComponent, {Product} from "../components/card.tsx";
+import { useEffect, useRef, useState } from "react";
+import ProductCardComponent, { Product } from "../components/card.tsx";
 import styled from "styled-components";
 
 export default function ProductsPage() {
@@ -7,6 +7,7 @@ export default function ProductsPage() {
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const pageRef = useRef<number>(1);
     const containerRef = useRef<HTMLDivElement | null>(null);
+    const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
 
     useEffect(() => {
         fetchProducts(pageRef.current);
@@ -15,10 +16,13 @@ export default function ProductsPage() {
 
     const fetchProducts = (page: number) => {
         setIsLoading(true);
-        fetch(`https://64ad67d8b470006a5ec5e9b1.mockapi.io/api/products/products?page=${page}&limit=8`)
+        fetch(
+            `https://64ad67d8b470006a5ec5e9b1.mockapi.io/api/products/products?page=${page}&limit=8`
+        )
             .then((response) => response.json())
-            .then((data) => {
-                setProducts((prevProducts) => [...prevProducts, ...data]);
+            .then((data: Product[]) => {
+                const sortedData = sortOrder === "asc" ? data.sort((a, b) => a.price - b.price) : data.sort((a, b) => b.price - a.price);
+                setProducts((prevProducts) => [...prevProducts, ...sortedData]);
                 setIsLoading(false);
             })
             .catch((error) => {
@@ -38,17 +42,29 @@ export default function ProductsPage() {
     };
 
     useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
     }, []);
 
+    const handleSortClick = () => {
+        setSortOrder((prevOrder) => (prevOrder === "asc" ? "desc" : "asc"));
+        setProducts([]); // Очищаем список товаров, чтобы обновить его заново с новым порядком сортировки.
+        pageRef.current = 1; // Сбрасываем номер страницы на первую.
+        fetchProducts(1); // Загружаем товары с первой страницы, они уже будут отсортированы в соответствии с sortOrder.
+    };
+
     return (
-        <ProductsContainer ref={containerRef}>
-            {products.map((product) => (
-                <ProductCardComponent key={product.id} product={product} />
-            ))}
-            {isLoading && <LoadingIndicator>Loading...</LoadingIndicator>}
-        </ProductsContainer>
+        <div>
+            <SortButton onClick={handleSortClick}>
+                Sort by Price ({sortOrder === "asc" ? "Low to High" : "High to Low"})
+            </SortButton>
+            <ProductsContainer ref={containerRef}>
+                {products.map((product) => (
+                    <ProductCardComponent key={product.id} product={product} />
+                ))}
+                {isLoading && <LoadingIndicator>Loading...</LoadingIndicator>}
+            </ProductsContainer>
+        </div>
     );
 }
 
@@ -81,5 +97,16 @@ const LoadingIndicator = styled.div`
   align-items: center;
   justify-content: center;
   height: 3rem;
+  font-weight: bold;
+`;
+
+const SortButton = styled.button`
+  margin-bottom: 1rem;
+  padding: 0.5rem 1rem;
+  background-color: #f0f0f0;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
   font-weight: bold;
 `;
