57c8c6aa28b26dc08b365049ff437586df9191e3
XnextOneO <xnextone@gmail.com>
2023-07-27 19:34:42 +0300

    feat add second slider(gender)

--- src/pages/calc-pc.tsx
+++ src/pages/calc-pc.tsx
@@ -1,4 +1,4 @@
-import{ useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Slider from '@mui/material/Slider';
 
@@ -8,6 +8,7 @@ interface Product {
     price: string;
     image: string;
     color: string;
+    gender: string;
 }
 
 const PRODUCTS_URL = 'https://64ad67d8b470006a5ec5e9b1.mockapi.io/api/products/products';
@@ -23,6 +24,7 @@ const ProductCard = ({ product }: { product: Product }) => (
 const CalcPc = () => {
     const [products, setProducts] = useState<Product[]>([]);
     const [selectedColorIndex, setSelectedColorIndex] = useState<number | null>(null);
+    const [selectedGenderIndex, setSelectedGenderIndex] = useState<number | null>(null);
 
     useEffect(() => {
         axios.get<Product[]>(PRODUCTS_URL).then((response) => {
@@ -31,21 +33,38 @@ const CalcPc = () => {
     }, []);
 
     const handleColorSliderChange = (event: Event, newValue: number | number[]) => {
-        console.log(event);
         if (typeof newValue === 'number') {
             setSelectedColorIndex(newValue);
         }
     };
 
+    const handleGenderSliderChange = (event: Event, newValue: number | number[]) => {
+        if (typeof newValue === 'number') {
+            setSelectedGenderIndex(newValue);
+        }
+    };
+
     // Только те цвета, которые были в исходном коде
     const availableColors = ['grey', 'turquoise', 'teal', 'salmon', 'tan'];
-    const marks = availableColors.map((color, index) => ({ value: index, label: color }));
+    const colorMarks = availableColors.map((color, index) => ({ value: index, label: color }));
+
+    const availableGenders = [
+        'Cis male',
+        'T* woman',
+        'Cis man',
+        'T* man',
+        'FTM',
+    ];
+    const genderMarks = availableGenders.map((gender, index) => ({ value: index, label: gender }));
 
     const selectedColor = selectedColorIndex !== null ? availableColors[selectedColorIndex] : null;
+    const selectedGender = selectedGenderIndex !== null ? availableGenders[selectedGenderIndex] : null;
 
-    const filteredProducts = selectedColor
-        ? products.filter((product) => product.color === selectedColor)
-        : products;
+    const filteredProducts = products.filter(
+        (product) =>
+            (selectedColor ? product.color === selectedColor : true) &&
+            (selectedGender ? product.gender === selectedGender : true)
+    );
 
     return (
         <div>
@@ -59,14 +78,34 @@ const CalcPc = () => {
                     step={1}
                     min={0}
                     max={availableColors.length - 1}
-                    marks={marks}
+                    marks={colorMarks}
                     valueLabelDisplay="on"
                 />
             </div>
             <div>
-                {filteredProducts.map((product) => (
-                    <ProductCard key={product.id} product={product} />
-                ))}
+                Выбор товара по гендеру
+                <div>
+                    <Slider
+                        defaultValue={0}
+                        value={selectedGenderIndex !== null ? selectedGenderIndex : 0}
+                        onChange={handleGenderSliderChange}
+                        aria-labelledby="discrete-slider-custom"
+                        step={1}
+                        min={0}
+                        max={availableGenders.length - 1}
+                        marks={genderMarks}
+                        valueLabelDisplay="on"
+                    />
+                </div>
+            </div>
+            <div>
+                {filteredProducts.length > 0 ? (
+                    filteredProducts.map((product) => (
+                        <ProductCard key={product.id} product={product} />
+                    ))
+                ) : (
+                    <p>Не найдено</p>
+                )}
             </div>
         </div>
     );
